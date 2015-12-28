ddb4c394a33d4c4db8834125f069f12bc17c44e4
Stefan Wille <github@stefanwille.com>
2015-12-29 00:37:38 +0100

    .

--- src/redis/protocol/marshal_test.go
+++ src/redis/protocol/marshal_test.go
@@ -6,6 +6,13 @@ import (
 	"testing"
 )
 
+func assertMarshalEquals(any Any, expected string, t *testing.T) {
+	result := marshal(any)
+	if result != expected {
+		t.Errorf("Expected %s, got %s", expected, result)
+	}
+}
+
 func marshal(any Any) string {
 	var buffer bytes.Buffer
 
@@ -16,41 +23,34 @@ func marshal(any Any) string {
 	return buffer.String()
 }
 
-func testMarshal(any Any, expected string, t *testing.T) {
-	result := marshal(any)
-	if result != expected {
-		t.Errorf("Expected %s, got %s", expected, result)
-	}
-}
-
 func TestMarshalInt(t *testing.T) {
-	testMarshal(1000, ":1000\r\n", t)
+	assertMarshalEquals(1000, ":1000\r\n", t)
 }
 
 func TestMarshalString(t *testing.T) {
-	testMarshal("foobar", "$6\r\nfoobar\r\n", t)
+	assertMarshalEquals("foobar", "$6\r\nfoobar\r\n", t)
 }
 
 func TestMarshalEmptyString(t *testing.T) {
-	testMarshal("", "$0\r\n\r\n", t)
+	assertMarshalEquals("", "$0\r\n\r\n", t)
 }
 
 func TestMarshalError(t *testing.T) {
-	testMarshal(errors.New("Error message"), "-Error message\r\n", t)
+	assertMarshalEquals(errors.New("Error message"), "-Error message\r\n", t)
 }
 
 func TestMarshalNil(t *testing.T) {
-	testMarshal(nil, "$-1\r\n", t)
+	assertMarshalEquals(nil, "$-1\r\n", t)
 }
 
 func TestMarshalArray(t *testing.T) {
 	var a []Any = []Any{"foo", "bar"}
-	testMarshal(&a, "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n", t)
+	assertMarshalEquals(&a, "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n", t)
 }
 
 func TestMarshalEmptyArray(t *testing.T) {
 	var a []Any = []Any{}
-	testMarshal(&a, "*0\r\n", t)
+	assertMarshalEquals(&a, "*0\r\n", t)
 }
 
 func TestMarshalUnsupportedTypeReturnsAnError(t *testing.T) {
