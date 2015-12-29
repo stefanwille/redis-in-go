25e105e7cd383e15bd4d5eecce53a1c2fb935d57
Stefan Wille <github@stefanwille.com>
2015-12-29 14:51:46 +0100

    .

---
+++ src/redis/server/requesthandlers/keys/keys_test.go
@@ -0,0 +1,35 @@
+package keys
+
+import (
+	"redis/protocol"
+	"redis/server/requesthandlers"
+	"reflect"
+	"testing"
+)
+
+func TestKeys_ReturnsAllKeys(t *testing.T) {
+	requestContext := requesthandlers.NewTestRequestContext()
+	requestContext.GetDatabase().Collections["key"] = "value"
+
+	response := Keys(requestContext, []protocol.Any{"KEYS", "*"})
+	if !reflect.DeepEqual(response, []protocol.Any{"key"}) {
+		t.Errorf("Expected response ['key'], got %v", response)
+		return
+	}
+}
+
+func TestKeys_FiltersByGlob(t *testing.T) {
+	requestContext := requesthandlers.NewTestRequestContext()
+	requestContext.GetDatabase().Collections["key"] = "value"
+
+	response := Keys(requestContext, []protocol.Any{"KEYS", "m*"})
+	sa, ok := response.([]protocol.Any)
+	if !ok {
+		t.Errorf("Expected response [], got %T ", response)
+	}
+
+	if len(sa) != 0 {
+		t.Errorf("Expected response [], got %v %d", sa, len(sa))
+		return
+	}
+}
