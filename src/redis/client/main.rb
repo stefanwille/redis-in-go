dcc618acdf4d4082b1084e79e667c0fc5d5a0048
Stefan Wille <github@stefanwille.com>
2015-12-29 15:14:04 +0100

    .

--- src/redis/client/main.rb
+++ src/redis/client/main.rb
@@ -2,9 +2,11 @@ require "redis"
 
 redis = Redis.new
 
-100_000.times do
-redis.set("mykey", "hello world")
-end
-# puts redis.set("yourkey", "goodbye world")
-# puts redis.get("mykey")
-# puts "keys:", redis.keys("*")
+# 100_000.times do
+# redis.set("mykey", "hello world")
+# end
+puts redis.set("mykey", "hello world")
+puts redis.get("mykey")
+puts "keys:", redis.keys("*")
+puts redis.del("mykey")
+puts "keys:", redis.keys("*")
